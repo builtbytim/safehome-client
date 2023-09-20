dfff54b498678e7ea094b6306086a84fef0f9b04
timmypelumy <timileyinpelumi.dev@gmail.com>
2023-09-20 08:39:49 +0100

    feat: improvs..

--- app/(kyc)/layout.jsx
+++ app/(kyc)/layout.jsx
@@ -1,5 +1,5 @@
 import { BsX } from "react-icons/bs";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Layout({ children }) {
   const router = useRouter();
