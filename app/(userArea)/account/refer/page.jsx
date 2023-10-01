117553795709e9f5df8dd91b42550c2423f268c6
akila-dev <akilapanunayan@gmail.com>
2023-10-01 22:00:50 +0100

    Account and Investment Update

--- app/(userArea)/account/refer/page.jsx
+++ app/(userArea)/account/refer/page.jsx
@@ -1,26 +1,74 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Image from "next/image";
 
-import { PiCaretUpBold, PiCaretDownBold } from "react-icons/pi";
+import { PiMoneyDuotone } from "react-icons/pi";
+import { FaMoneyBill } from "react-icons/fa";
+import { HiOutlineUsers } from "react-icons/hi2";
 import picIcon from "../../../../assets/images/icons/picIcon.svg";
+import CopyButton from "../../../components/CopyButton.jsx";
+
+const referralsData = [
+	{
+		date: "6th September, 2023",
+		name: "Stephen Jobs",
+	},
+	{
+		date: "6th September, 2023",
+		name: "John Doe",
+	},
+	{
+		date: "6th September, 2023",
+		name: "Francis Drake",
+	},
+];
 
 export default function Refer() {
-	const [isHidden, setIsHidden] = useState(true);
-	const fileRef = useRef(null);
-
-	const openFile = () => {
-		// `current` points to the mounted file input element
-		fileRef.current.click();
-	};
 	return (
-		<main className=" space-y-8 lg:space-y-8 text-[--text-secondary] border border-[--lines] p-5 h-full min-h-[80vh] rounded-2xl">
+		<main className=" space-y-2 lg:space-y-3 text-[--text-secondary] border border-[--lines] p-5 h-full min-h-[80vh] rounded-2xl">
 			<div className="pb-3 space-y-2">
 				<h3 className="text-3xl text-[--color-brand] font-semibold">
 					Referrals
 				</h3>
 				<p>Refer and earn 40% per referral.</p>
+			</div>
+			<div>
+				<div className="flex justify-end">
+					<button className="bg-[--color-brand] flex text-white py-3 px-6 rounded-lg items-center gap-2">
+						<FaMoneyBill className="text-white text-2xl" /> Withdraw
+					</button>
+				</div>
+				{/* Cards */}
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-7 text-[--color-brand]">
+					<div className="w-full h-[150px] flex flex-col justify-between p-5 bg-[--card-bg-1] rounded-xl">
+						<span>
+							<PiMoneyDuotone className="text-[--color-brand] text-lg" />
+						</span>
+						<p className="text-sm">Total Earned</p>
+						<p className="text-xl font-semibold">₦5,000</p>
+					</div>
+
+					<div className="w-full h-[150px] flex flex-col justify-between p-5 bg-[--card-bg-2] rounded-xl">
+						<span>
+							<HiOutlineUsers className="text-[--color-brand] text-lg" />
+						</span>
+						<p className="text-sm">Referrals</p>
+						<p className="text-xl font-semibold">{referralsData.length}</p>
+					</div>
+
+					{/* Copy buttons */}
+					<CopyButton
+						link="https://www.google.com/"
+						type="link"
+						additionalClasses=""
+					/>
+					<CopyButton
+						link="ITEHHOH"
+						type="code"
+						additionalClasses="text-right"
+					/>
+				</div>
 			</div>
 		</main>
 	);
--- app/components/security/index.js
+++ app/components/security/index.js
@@ -2,7 +2,4 @@ export { default as Authenticator } from "./Authenticator.jsx";
 export { default as SMSAuthenticator } from "./SMSAuthenticator.jsx";
 export { default as ToggleCard } from "./ToggleCard.jsx";
 export { default as PopUpTopBar } from "./PopUpTopBar.jsx";
-<<<<<<< HEAD
 export { default as SecurityQuestionAuth } from "./SecurityQuestionAuth.jsx";
-=======
->>>>>>> a093b9abec02d8f87703b932daae9f59e68fe111
