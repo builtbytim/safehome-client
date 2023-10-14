1f08e475f7c8faca6410e35a74f0c1aeed6be542
timmypelumy <timileyinpelumi.dev@gmail.com>
2023-10-14 10:56:05 +0100

    fixes

--- app/components/investment/InvestmentCard.jsx
+++ app/components/investment/InvestmentCard.jsx
@@ -1,59 +1,53 @@
 "use client";
 
-import React from "react";
 import Image from "next/image";
 
 const InvestmentCard = ({
-	img,
-	title,
-	returns,
-	value,
-	investors,
-	quantity,
-	openInfo,
+  img,
+  title,
+  returns,
+  value,
+  investors,
+  quantity,
+  openInfo,
 }) => {
-	return (
-		<button
-			className="relative rounded md:rounded-lg lg:rounded-xl border overflow-hidden text-[--text-secondary] text-left grid grid-cols-5 md:grid-cols-5 h-[165px] md:h-[192px] w-full group"
-			onClick={() => openInfo()}
-		>
-			<div className="w-full h-full col-span-2 md:col-span-2 overflow-x-hidden">
-				<Image
-					src={img}
-					alt={title}
-					className="object-cover w-full h-full"
-					width={200}
-					height={192}
-				/>
-			</div>
-			<div className="p-3 md:px-4 xl:px-7 col-span-3 md:col-span-3 space-y-1">
-				<button className="uppercase font-medium text-[1rem] leading-[1.2rem] md:text-xl md:leading-[1.4rem] xl:text-[1.35rem] xl:leading-[1.55rem] max-h-[2.4rem] md:max-h-[2.8rem] xl:max-h-[3.1rem] text-left overflow-hidden group-hover:text-[--text-brand]">
-					{title}
-				</button>
-				<p className="text-[0.75rem] leading-[1] md:text-sm">
-					<span className="text-[--green] font-medium">{returns}</span> returns
-					in 12 months
-				</p>
-				<div className="flex gap-3 md:gap-5 pt-3 text-[0.75rem] leading-[1] md:text-sm">
-					<div>
-						<h3 className="text-[--text-brand] text-[1rem] leading-[1.2rem] md:text-xl pb-1 font-medium">
-							₦{value}
-						</h3>
-						<p className="mt-[-4px]">Asset Value</p>
-					</div>
-					<div>
-						<h3 className="text-[--text-brand] text-[1rem] leading-[1.2rem] md:text-xl pb-1 font-medium">
-							{investors}
-						</h3>
-						<p className="mt-[-4px]">Investors</p>
-					</div>
-				</div>
-			</div>
-			<div className="absolute bottom-0 right-0 rounded-tl-xl py-[0.4rem] md:py-2 px-3 text-white bg-[--green] text-[0.7rem] md:text-sm">
-				{quantity} Available
-			</div>
-		</button>
-	);
+  return (
+    <button
+      className="relative rounded-brand md:rounded-lg lg:rounded-xl border overflow-hidden text-[--text-secondary] text-left grid grid-cols-5 md:grid-cols-5 h-[165px] md:h-[192px] w-full group text-sm transitioning"
+      onClick={() => openInfo()}
+    >
+      <div className="w-full h-full col-span-2 md:col-span-2 overflow-x-hidden">
+        <Image
+          src={img}
+          alt={title}
+          className="object-cover w-full h-full"
+          width={200}
+          height={192}
+        />
+      </div>
+      <div className="p-3 md:px-4 xl:px-7 col-span-3 md:col-span-3 space-y-1">
+        <button className="uppercase font-medium text-lg md:text-xl text-left truncate group-hover:text-[--text-brand]">
+          {title}
+        </button>
+
+        <div className="flex gap-4 md:gap-6 pt-2 ">
+          <div>
+            <h3 className="text-[--text-brand]   pb-1 font-medium">₦{value}</h3>
+            <p className="mt-[-4px]">Asset Value</p>
+          </div>
+          <div>
+            <h3 className="text-[--text-brand]   pb-1 font-medium">
+              {investors}
+            </h3>
+            <p className="mt-[-4px]">Investors</p>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 right-0 rounded-tl-xl py-[0.4rem] md:py-2 px-3 text-white bg-[--green] text-[0.7rem] md:text-sm">
+        {quantity} Available
+      </div>
+    </button>
+  );
 };
 
 export default InvestmentCard;
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import "./globals.css";
-import { Outfit } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import config from "./utils/config";
 import Notify from "./components/Notify";
 import Notifications from "./components/Notifications";
 import RemoteDataProvider from "./utils/RemoteDataProvider";
 
-const outfit = Outfit({ subsets: ["latin"] });
+const noto = Noto_Sans({ subsets: ["cyrillic"] });
 
 export const metadata = {
   title: config.app.name,
@@ -16,7 +16,7 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={outfit.className}>
+      <body className={noto.className}>
         <RemoteDataProvider>
           <Notify />
           <Notifications />
