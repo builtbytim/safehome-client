09968f69f8609ad49b18d8ec91dba2e1e35efca3
timmypelumy <timileyinpelumi.dev@gmail.com>
2023-10-14 07:05:59 +0100

    fixes

--- app/(userArea)/page.jsx
+++ app/(userArea)/page.jsx
@@ -182,8 +182,8 @@ function Page({ authenticatedUser, authenticationToken }) {
         />
 
         <ExploreCardsRow user={authenticatedUser} />
-        <section className="bg-white rounded-brand pb-8  space-y-4">
-          <div className="flex flex-row justify-between items-center pt-8 md:px-8">
+        <section className="bg-white rounded-brand pb-8  space-y-4 ">
+          <div className="flex flex-row justify-between items-center pt-8 md:px-8  ">
             <div className="flex justify-start items-center space-x-16">
               <h1 className="  text-[--placeholder] md:text-[--text-secondary] capitalize text-xl sm:text-2xl md:text-3xl lg:text-3xl  font-medium">
                 Transactions
