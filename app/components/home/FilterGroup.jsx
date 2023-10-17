03764f92d094a1bb0c5cbab473674a45f6c7a506
Akilapa Godspower <akilapanunayan@gmail.com>
2023-10-17 15:03:59 +0100

    Savings Update (#17)

--- app/components/home/FilterGroup.jsx
+++ app/components/home/FilterGroup.jsx
@@ -4,12 +4,16 @@ import DateFilter from "./DateFilter";
 import TransactionTypeFilter from "./TransactionTypeFilter";
 
 function FilterGroup({ setDateFilter, setTxTypeFilter }) {
-  return (
-    <div className=" flex flex-row justify-start items-center space-x-4 md:space-x-16  text-[--sorta-dark]  no-scrollbar  max-w-full  ">
-      <TransactionTypeFilter setTxTypeFilter={setTxTypeFilter} />
-      <DateFilter setDateFilter={setDateFilter} />
-    </div>
-  );
+	return (
+		<div className=" flex flex-wrap lg:justify-end items-center gap-3 lg:gap-5 w-full max-w-full text-sm lg:text-base">
+			<div>
+				<TransactionTypeFilter setTxTypeFilter={setTxTypeFilter} />
+			</div>
+			<div>
+				<DateFilter setDateFilter={setDateFilter} />
+			</div>
+		</div>
+	);
 }
 
 export default FilterGroup;
