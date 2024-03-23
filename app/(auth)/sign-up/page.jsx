172bc9496bf3b265acbe64aa772c6fc448673fdd
unknown <akilapanunayan@gmail.com>
2024-03-23 22:24:49 +0100

    Corrections

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -3,27 +3,27 @@ import Link from "next/link";
 import config from "../../utils/config.js";
 
 export const metadata = {
-  title: `Sign up | ${config.app.name}`,
+	title: `Sign up | ${config.app.name}`,
 };
 
 export default function Page() {
-  return (
-    <>
-      <div className="bg-white relative  w-[448px] max-w-[95%] sm:max-w-[80%] lg:max-w-sm min-h-[300px] rounded-brand py-8 px-4 sm:p-8 ">
-        <SignUp />
-      </div>
+	return (
+		<>
+			<div className="bg-white relative  w-[448px] max-w-[95%] sm:max-w-[80%] lg:max-w-sm min-h-[300px] rounded-brand py-8 px-4 sm:p-8 ">
+				<SignUp />
+			</div>
 
-      <div className="mt-8 space-y-2 lg:space-y-4">
-        <p className="text-center text-sm text-[--color-brand-2] sm:text-white ">
-          Already have an account?{" "}
-          <Link
-            href="/sign-in"
-            className="text-[--color-brand-2] sm:text-white font-semibold sm:hover:text-[--lines] cursor-pointer "
-          >
-            Log in
-          </Link>
-        </p>
-      </div>
-    </>
-  );
+			<div className="mt-8 pb-5 space-y-2 lg:space-y-4">
+				<p className="text-center text-sm text-[--color-brand-2] sm:text-white ">
+					Already have an account?{" "}
+					<Link
+						href="/sign-in"
+						className="text-[--color-brand-2] sm:text-white font-semibold sm:hover:text-[--lines] cursor-pointer "
+					>
+						Log in
+					</Link>
+				</p>
+			</div>
+		</>
+	);
 }
--- app/components/forms/SignIn.jsx
+++ app/components/forms/SignIn.jsx
@@ -12,204 +12,198 @@ import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 import { useState } from "react";
 
 function SignIn() {
-  const setNotify = useNotifyStore((state) => state.setNotify);
-  const [showPassword, setShowPassword] = useState(false);
+	const setNotify = useNotifyStore((state) => state.setNotify);
+	const [showPassword, setShowPassword] = useState(false);
 
-  const searchParams = useSearchParams();
+	const searchParams = useSearchParams();
 
-  const mode =
-    searchParams.get(config.signInModeParam) || config.signInModes.REGULAR;
+	const mode =
+		searchParams.get(config.signInModeParam) || config.signInModes.REGULAR;
 
-  function toggleShowPassword() {
-    setShowPassword(!showPassword);
-  }
+	function toggleShowPassword() {
+		setShowPassword(!showPassword);
+	}
 
-  const router = useRouter();
+	const router = useRouter();
 
-  function onError(err, vars) {
-    const action = err.action;
+	function onError(err, vars) {
+		const action = err.action;
 
-    switch (action) {
-      case "VERIFY_EMAIL":
-        setNotify({
-          show: true,
-          title: "Unable to sign in",
-          content: err?.message,
-          onAcceptText: "Verify Email",
-          onAccept: () => {
-            router.push(
-              `/verify-email/${vars.username}?authCode=${err?.authCode}`
-            );
-          },
-        });
+		switch (action) {
+			case "VERIFY_EMAIL":
+				setNotify({
+					show: true,
+					title: "Unable to sign in",
+					content: err?.message,
+					onAcceptText: "Verify Email",
+					onAccept: () => {
+						router.push(
+							`/verify-email/${vars.username}?authCode=${err?.authCode}`
+						);
+					},
+				});
 
-        break;
+				break;
 
-      case "VERIFY_KYC":
-        setNotify({
-          show: true,
-          title: "Unable to sign in",
-          content: err?.message,
-          onAcceptText: "Verify KYC",
-          onAccept: () => {
-            router.push(`/kyc?authCode=${err?.authCode}`);
-          },
-        });
+			case "VERIFY_KYC":
+				setNotify({
+					show: true,
+					title: "Unable to sign in",
+					content: err?.message,
+					onAcceptText: "Verify KYC",
+					onAccept: () => {
+						router.push(`/kyc?authCode=${err?.authCode}`);
+					},
+				});
 
-        break;
+				break;
 
-      default:
-        setNotify({
-          show: true,
-          title: "Unable to sign in",
-          content: err?.message,
-        });
-        reset();
-        break;
-    }
-  }
+			default:
+				setNotify({
+					show: true,
+					title: "Unable to sign in",
+					content: err?.message,
+				});
+				reset();
+				break;
+		}
+	}
 
-  function onSuccess(data) {
-    const tokenObj = {
-      ...data,
+	function onSuccess(data) {
+		const tokenObj = {
+			...data,
 
-      createdAt: Date.now(),
-    };
+			createdAt: Date.now(),
+		};
 
-    saveToLocalStorage(tokenObj, `${config.localStorageKey}:token`);
+		saveToLocalStorage(tokenObj, `${config.localStorageKey}:token`);
 
-    if (searchParams.has(config.redirectSearchParam)) {
-      let targetUrl = searchParams.get(config.redirectSearchParam);
+		if (searchParams.has(config.redirectSearchParam)) {
+			let targetUrl = searchParams.get(config.redirectSearchParam);
 
-      const decodedUrl = decodeURIComponent(targetUrl);
+			const decodedUrl = decodeURIComponent(targetUrl);
 
-      if (config.signInRedirectIgnore.includes(decodedUrl)) {
-        router.push(config.authenticatedHome);
-        return;
-      }
+			if (config.signInRedirectIgnore.includes(decodedUrl)) {
+				router.push(config.authenticatedHome);
+				return;
+			}
 
-      router.push(decodedUrl);
-      return;
-    }
+			router.push(decodedUrl);
+			return;
+		}
 
-    router.push(config.authenticatedHome);
-  }
+		router.push(config.authenticatedHome);
+	}
 
-  const { mutate, isLoading, reset } = useSignIn(onSuccess, onError);
+	const { mutate, isLoading, reset } = useSignIn(onSuccess, onError);
 
-  function handleSignIn(values) {
-    if (isLoading) return;
+	function handleSignIn(values) {
+		if (isLoading) return;
 
-    const body = {
-      username: values.email,
-      password: values.password,
-    };
+		const body = {
+			username: values.email,
+			password: values.password,
+		};
 
-    mutate(body);
-  }
+		mutate(body);
+	}
 
-  return (
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      validationSchema={Yup.object({
-        email: Yup.string().email("Invalid email address").required("Required"),
-        password: Yup.string()
-          .min(8, "Must be 8 characters or more")
-          .required("Required"),
-      })}
-      onSubmit={handleSignIn}
-    >
-      {({ isValid }) => (
-        <Form className="flex  flex-col border-0 justify-center items-center space-y-4">
-          <BarLoader active={isLoading} />
+	return (
+		<Formik
+			initialValues={{
+				email: "",
+				password: "",
+			}}
+			validationSchema={Yup.object({
+				email: Yup.string().email("Invalid email address").required("Required"),
+				password: Yup.string()
+					.min(8, "Must be 8 characters or more")
+					.required("Required"),
+			})}
+			onSubmit={handleSignIn}
+		>
+			{({ isValid }) => (
+				<Form className="flex  flex-col border-0 justify-center items-center space-y-4">
+					<BarLoader active={isLoading} />
 
-          <div>
-            <legend className="font-bold text-center text-lg lg:text-xl text-[--color-brand]">
-              {mode === config.signInModes.AUTH_FAILED &&
-                "Session Expired, Log in again"}
-              {mode === config.signInModes.REGULAR && "Welcome Back"}
+					<div>
+						<legend className="font-bold text-center text-lg lg:text-xl text-[--header]">
+							{mode === config.signInModes.AUTH_FAILED &&
+								"Session Expired, Log in again"}
+							{mode === config.signInModes.REGULAR && "Welcome Back"}
 
-              {mode === config.signInModes.NEW_USER && "Log in to your account"}
-              {!Object.values(config.signInModes).includes(mode) &&
-                "Welcome Back"}
-            </legend>
-            <p className="text-center text-xs text-[--text-secondary] ">
-              {" "}
-              Securely log in to your SafeHome{" "}
-            </p>
-          </div>
+							{mode === config.signInModes.NEW_USER && "Log in to your account"}
+							{!Object.values(config.signInModes).includes(mode) &&
+								"Welcome Back"}
+						</legend>
+						<p className="text-center text-xs">
+							{" "}
+							Securely log in to your SafeHome{" "}
+						</p>
+					</div>
 
-          <div className="flex flex-col justify-center items-center space-y-6 w-full">
-            <div className="w-full relative flex flex-col justify-center items-start space-y-2">
-              <label
-                htmlFor="email"
-                className="text-[--text-secondary] text-sm text-left"
-              >
-                Email Address
-              </label>
+					<div className="flex flex-col justify-center items-center space-y-6 w-full">
+						<div className="w-full relative flex flex-col justify-center items-start space-y-2">
+							<label htmlFor="email" className="text-sm text-left">
+								Email Address
+							</label>
 
-              <Field
-                name="email"
-                type="email"
-                className="field-1"
-                placeholder="Email"
-              />
+							<Field
+								name="email"
+								type="email"
+								className="field-1"
+								placeholder="Email"
+							/>
 
-              <ErrorMessage
-                name="email"
-                component="div"
-                className="absolute -bottom-[30%] left-0 text-[--text-danger] text-xs text-left"
-              />
-            </div>
+							<ErrorMessage
+								name="email"
+								component="div"
+								className="absolute -bottom-[30%] left-0 text-[--text-danger] text-xs text-left"
+							/>
+						</div>
 
-            <div className="w-full relative flex flex-col justify-center items-start space-y-2">
-              <label
-                htmlFor="password"
-                className="text-[--text-secondary] text-sm text-left"
-              >
-                Password
-              </label>
+						<div className="w-full relative flex flex-col justify-center items-start space-y-2">
+							<label htmlFor="password" className="text-sm text-left">
+								Password
+							</label>
 
-              <div className="relative w-full">
-                <Field
-                  name="password"
-                  type={showPassword ? "text" : "password"}
-                  className="field-1"
-                  placeholder="Password"
-                />
+							<div className="relative w-full">
+								<Field
+									name="password"
+									type={showPassword ? "text" : "password"}
+									className="field-1"
+									placeholder="Password"
+								/>
 
-                <div
-                  className="absolute top-[50%] -translate-y-[50%] right-[5%] text-[--text-secondary]  text-right cursor-pointer text-xl"
-                  onClick={toggleShowPassword}
-                >
-                  {showPassword ? <BsEyeSlashFill /> : <BsEyeFill />}
-                </div>
-              </div>
+								<div
+									className="absolute top-[50%] -translate-y-[50%] right-[5%]  text-right cursor-pointer text-xl"
+									onClick={toggleShowPassword}
+								>
+									{showPassword ? <BsEyeSlashFill /> : <BsEyeFill />}
+								</div>
+							</div>
 
-              <ErrorMessage
-                name="password"
-                component="div"
-                className="absolute -bottom-[30%] left-0 text-[--text-danger] text-xs text-left"
-              />
-            </div>
-          </div>
+							<ErrorMessage
+								name="password"
+								component="div"
+								className="absolute -bottom-[30%] left-0 text-[--text-danger] text-xs text-left"
+							/>
+						</div>
+					</div>
 
-          <div className="flex flex-col justify-center items-center w-full pt-8 space-y-4 ">
-            <button
-              disabled={!isValid || isLoading}
-              type="submit"
-              className="btn-1"
-            >
-              Log in
-            </button>
-          </div>
-        </Form>
-      )}
-    </Formik>
-  );
+					<div className="flex flex-col justify-center items-center w-full pt-8 space-y-4 ">
+						<button
+							disabled={!isValid || isLoading}
+							type="submit"
+							className="btn-1"
+						>
+							Log in
+						</button>
+					</div>
+				</Form>
+			)}
+		</Formik>
+	);
 }
 
 export default SignIn;
